855ea809e90a458d2951e334bec25a881111c886
phoenix104104 <phoenix104104@gmail.com>
2015-05-04 14:58:29 +0800

    add list

--- svm-c/random_sample.py
+++ svm-c/random_sample.py
@@ -1,20 +1,20 @@
 import numpy as np
 import random, os
 
-N = 3696
+N = 462
 train_num = 400
-valid_num = 400
+valid_num = 62
 
 sample_index = random.sample(range(N), train_num + valid_num)
-train_index = sample_index[:train_num]
-valid_index = sample_index[train_num+1:]
+train_index = sorted(sample_index[:train_num])
+valid_index = sorted(sample_index[train_num:])
 
-output_dir = '../../list'
-filename = os.path.join(output_dir, 'train400.list')
+output_dir = '.'
+filename = os.path.join(output_dir, 'train%d.list' %train_num)
 print "Save %s" %filename
 np.savetxt(filename, train_index, fmt='%d')
 
-filename = os.path.join(output_dir, 'valid400.list')
+filename = os.path.join(output_dir, 'valid%d.list' %train_num)
 print "Save %s" %filename
 np.savetxt(filename, valid_index, fmt='%d')
 
--- svm-c/sample_data.py
+++ svm-c/sample_data.py
@@ -3,63 +3,81 @@ import random, os
 
 def extract_audio_name(string):
     s = string.split('_')
-    name = s[0] + '_' + s[1]
-    return name
+    person = s[0]
+    audio = s[1]
+    return person, audio
 
-n_sample = 400
-list_dir = '../../list'
-filename = os.path.join(list_dir, 'train%d.list' %n_sample)
-print "Load %s" %filename
-train_index = np.loadtxt(filename, dtype='int')
 
-filename = os.path.join(list_dir, 'valid%d.list' %n_sample)
-print "Load %s" %filename
-valid_index = np.loadtxt(filename, dtype='int')
 
-feature_dir = '../../feature'
-fv_name = 'fbank'
-
-feature_filename = os.path.join(feature_dir, 'train.%s' %fv_name)
-train_filename = os.path.join(feature_dir, 'train%d.%s' %(n_sample, fv_name) )
-valid_filename = os.path.join(feature_dir, 'valid%d.%s' %(n_sample, fv_name) )
-
-audio_all = []
-n_audio = 0
-with open(feature_filename, 'r') as f:
-    print "Load %s" %feature_filename
-    current_name = ""
-    for line in f.readlines():
-        s = line.rstrip().split()
-        audio_name = extract_audio_name(s[0])
-        if( audio_name != current_name ):
-            current_name = audio_name
-            if( n_audio > 0 ):
-                audio_all.append(frame_list)
-
-            frame_list = []
-            n_audio += 1
-
-        frame_list.append(line)
+if __name__ == '__main__':
     
-    audio_all.append(frame_list)
+    feature_dir = '../../feature'
+    fv_name = 'fbank'
+
+
+    feature_filename = os.path.join(feature_dir, 'train.%s' %fv_name)
     
-audio_train = []
-for index in train_index:
-    audio_train.append(audio_all[index])
+    n_sample = 400
+    filename = 'train%d.list' %n_sample
+    print "Load %s" %filename
+    train_index = np.loadtxt(filename, dtype='int')
+    
+    filename = 'valid%d.list' %n_sample
+    print "Load %s" %filename
+    valid_index = np.loadtxt(filename, dtype='int')
+    
+    # output filename
+    train_filename = os.path.join(feature_dir, 'train%d.%s' %(n_sample, fv_name) )
+    valid_filename = os.path.join(feature_dir, 'valid%d.%s' %(n_sample, fv_name) )
+    
+    n_person = 0
+    n_audio = 0
+    person_all = []
+    with open(feature_filename, 'r') as f:
+        print "Load %s" %feature_filename
+        current_person = ""
+        current_audio = ""
+        for line in f.readlines():
+            s = line.rstrip().split()
+            person, audio = extract_audio_name(s[0])
+            if( person != current_person ):
+                #print "person = %s, n_audio = %d" %(person, n_audio)
+                current_person = person
+                
+                if( n_person > 0 ):
+                    person_all.append(audio_list)
 
-audio_valid = []
-for index in valid_index:
-    audio_valid.append(audio_all[index])
+                audio_list = []
+                n_person += 1
+                n_audio = 0
+                
+            
+            if( audio != current_audio ):
+                current_audio = audio
+                n_audio += 1
+    
+            audio_list.append(line)
+        
+        person_all.append(audio_list)
+        
+    print "Total person = %d" %len(person_all)
 
-with open(train_filename, 'w') as f:
-    print "Save %s" %train_filename
-    for audio in audio_train:
-        for frame in audio:
-            f.write(frame)
-
-with open(valid_filename, 'w') as f:
-    print "Save %s" %valid_filename
-    for audio in audio_valid:
-        for frame in audio:
-            f.write(frame)
-
+    person_train = []
+    for index in train_index:
+        person_train.append(person_all[index])
+    
+    person_valid = []
+    for index in valid_index:
+        person_valid.append(person_all[index])
+    
+    with open(train_filename, 'w') as f:
+        print "Save %s" %train_filename
+        for person in person_train:
+            for line in person:
+                f.write(line)
+    
+    with open(valid_filename, 'w') as f:
+        print "Save %s" %valid_filename
+        for person in person_valid:
+            for line in person:
+                f.write(line)
