29bbdd765e0dd1abd328f9c09d88494ae0e80c36
antevasin <denniswang6313@hotmail.com>
2015-05-04 17:35:48 +0800

    preprocess + add_one & normalize

--- svm-c/preprocess.py
+++ svm-c/preprocess.py
@@ -1,8 +1,15 @@
 import os
 from operator import itemgetter
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', action='store_true', help='add 1 to feature')
+parser.add_argument('-n', action='store_true', help='do normalization')
+parser.add_argument('-mfcc', action='store_true', help='create mfcc feature')
+
+args = parser.parse_args()
 
 path = '../../'
-
 
 label_list = []
 file = open(path+'48_idx_chr.map_b')
@@ -19,15 +26,33 @@ file = open(path+'label/train.lab')
 all_label = [split_label_line(line) for line in file]
 file.close()
 
+def normalize(lines) :
+	feature = [[float(i) for i in line[1].split()] for line in lines]
+
+	dim = len(feature[0])
+	x = [.0] * dim
+	x2 = [.0] * dim
+	for f in feature :
+		x = [x[i] + f[i] for i in range(dim)]
+		x2 = [x2[i] + pow(f[i],2) for i in range(dim)]
+
+	total = len(feature)
+	mean = [i/total for i in x]
+	var = [x2[i]/total - pow(mean[i], 2) for i in range(dim)]
+	
+	return [[lines[i][0]] + [str((feature[i][j] - mean[j])/var[j]) for j in range(dim)] for i in range(total)]
+
+
 all_label = sorted(all_label, key=itemgetter(0,1))
 
 if not os.path.exists(path+'feature') :
 	os.makedirs(path+'feature')
 
-for feature_type in ['fbank', 'mfcc'] :
+# train
+for feature_type in ['fbank', 'mfcc'] if args.mfcc else ['fbank']:
 	
 	ipath = path+'%s/train.ark' %feature_type
-	opath = path+'feature/train.%s' %feature_type
+	opath = path+'feature/train.%s' %feature_type + '.norm'*args.n + '.add'*args.a
 	if os.path.exists(opath) :
 		print 'skip file %s, already exists' %opath
 		continue
@@ -35,20 +60,28 @@ for feature_type in ['fbank', 'mfcc'] :
 	ifile = open(ipath)
 	ofile = open(opath, 'w+')
 
-	for label in all_label :
+	lines = [x.rstrip().split(None, 1) for x in ifile]
+
+	if args.n :
+		lines = normalize(lines)
+
+	if args.a :
+		lines = [line + ['1.0'] for line in lines]
+
+	assert len(all_label) == len(lines)
+	for label, line in zip(all_label, lines) :
 		filename = label[0] + '_' + str(label[1])
 		index = str(label[2])
 
-		line = ifile.readline().split(None, 1)
 		assert filename == line[0]
 		line.insert(1, index)
-		ofile.write(' '.join(line))
+		ofile.write(' '.join(line)+'\n')
 
-
-for feature_type in ['fbank', 'mfcc'] :
+# test
+for feature_type in ['fbank', 'mfcc'] if args.mfcc else ['fbank']:
 	
 	ipath = path+'%s/test.ark' %feature_type
-	opath = path+'feature/test.%s' %feature_type
+	opath = path+'feature/test.%s' %feature_type + '.norm'*args.n + '.add'*args.a
 	if os.path.exists(opath) :
 		print 'skip file %s, already exists' %opath
 		continue
@@ -56,8 +89,14 @@ for feature_type in ['fbank', 'mfcc'] :
 	ifile = open(ipath)
 	ofile = open(opath, 'w+')
 
-	for line in ifile :
-		s = line.split(None, 1)
-		s.insert(1, '0')
-		ofile.write(' '.join(s))
+	lines = [x.rstrip().split(None, 1) for x in ifile]
 
+	if args.n :
+		lines = normalize(lines)
+
+	if args.a :
+		lines = [line + ['1.0'] for line in lines]
+
+	for line in lines :
+		line.insert(1, '0')
+		ofile.write(' '.join(line) + '\n')
