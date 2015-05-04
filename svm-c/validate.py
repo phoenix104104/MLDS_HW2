795275f2f3e06ac8e5bca812d237f65f5935452e
Andy <ylldummy@gmail.com>
2015-05-04 21:55:52 +0800

    Fix bug in validate.py

--- svm-c/validate.py
+++ svm-c/validate.py
@@ -6,7 +6,7 @@ def edit_dist(a,b):
     present = range(0, len(b)+1)
     for i in range(1, len(a)+1):
         prev = present
-        present = [0]
+        present = [(i-1)]
         for j in range(1, len(b)+1):
             if(a[i-1] == b[j-1]):
                 present.append(prev[j-1])
